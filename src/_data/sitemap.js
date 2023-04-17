6e80358754a4d5c116d7da77da4f540ada687551
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-04-17 00:19:08 -0700

    Create site header

---
+++ src/_data/sitemap.js
@@ -0,0 +1,7 @@
+module.exports = [
+    '/khushmeet',
+    '/dogsheep',
+    '/current',
+    '/blog',
+    '/gpg'
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["src/**/*.{njk,md,html}", "src/**/*.svg"],
   theme: {
     extend: {},
+    fontFamily: {
+      'mono': ["Berkeley Mono", "monospace"],
+    }
   },
   plugins: [],
 }
