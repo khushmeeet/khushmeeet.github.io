85e9d43a2f09dac1340613543bf31fbb20cd1097
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-04-17 23:53:13 -0700

    Created main section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["src/**/*.{njk,md,html}", "src/**/*.svg"],
+  darkMode: 'class',
   theme: {
     extend: {},
     fontFamily: {
