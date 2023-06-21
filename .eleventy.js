5a5deaf1396e2987a58028a3ed71948abbe1571b
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2023-06-21 00:48:50 -0700

    Created infra page

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/js")
   eleventyConfig.addPassthroughCopy("src/assets")
   eleventyConfig.setBrowserSyncConfig({ open: true })
+  eleventyConfig.setDataDeepMerge(true);
+
+  let now = new Date();
+  let formattedDate = now.getFullYear() + "-" + ("0" + (now.getMonth() + 1)).slice(-2) + "-" + ("0" + now.getDate()).slice(-2);
+  eleventyConfig.addGlobalData("lastUpdated", `${formattedDate}`);
+
   return {
     templateFormats: [
       "md",
